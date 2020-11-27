18845088bd5fd8e7349cc85a8b9537f0061f5695
Carola Casais <carola.casais@gmail.com>
2020-11-27 13:18:08 +0100

    Add project files.

---
+++ CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CakeStoreApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeStoreApi.Controllers
+{
+	public class CakeStoreApiController : Controller
+	{
+		public IActionResult Index()
+		{
+
+            private IData _data;
+
+        public CakeStoreApiController(IData data)
+        {
+            _data = data;
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}", Name = "GetCake")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+			return View();
+		}
+	}
+}
